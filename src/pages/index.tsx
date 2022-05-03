7d88b13d1d4c4a32df1c117c25f69038d67e94ed
galnir <nirnir122@gmail.com>
2022-05-03 23:47:37 +0300

    add site title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import App from "../components/App";
 import GlobalStyles from "../components/GlobalStyles";
+import { Helmet } from "react-helmet";
 
 const IndexPage = () => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8"></meta>
+        <title>Master Bot</title>
+        <link rel="canonical" href="https://masterbotwebsitemain.gatsbyjs.io" />
+      </Helmet>
       <App />
       <GlobalStyles />
     </>
